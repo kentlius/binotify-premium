b248cbe8750c8fae1736fc4df269239c2bd5e5f0
Aldwin <13520167@std.stei.itb.ac.id>
2022-12-01 16:03:34 +0700

    feat: migrate login to mui

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,5 +1,6 @@
 import { Form, redirect } from "react-router-dom";
 import { JWTAuth } from "../users";
+import binotify from '../assets/binotify.png'
 
 export async function action({ request, params }) {
   const formData = await request.formData();
@@ -12,27 +13,76 @@ export async function action({ request, params }) {
   }
 }
 
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
 export default function Login() {
+
   return (
-    <>
-      <Form method="post" id="login-form">
-        <label>
-          <span>Username</span>
-          <input type="text" name="username" placeholder="Username" required />
-        </label>
-        <label>
-          <span>Password</span>
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            required
-          />
-        </label>
-        <p>
-          <button type="submit">Login</button>
-        </p>
-      </Form>
-    </>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <img src={binotify} alt="logo" width="40px" />
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component={Form} method="post" id="login-form">
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
